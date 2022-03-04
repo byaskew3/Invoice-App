e42cf6320f0627f27d9cbc72d3880ce077d4da19
byaskew3 <byaskew3@gmail.com>
2022-03-04 14:57:22 -0700

    JS

---
+++ app.js
@@ -0,0 +1,34 @@
+// --------- CACHING THE DOM ----------------
+
+const midContainer = document.querySelector('.mid-container');
+
+
+// --------------- GLOBAL VARIABLES --------------
+
+const servicesArr = ['Wash Car: $10', 'Mow Lawn: $20', 'Pull Weeds: $30']
+let serviceItems = [];
+
+// ---------------- EVENT LISTENERS ---------------------
+
+
+
+// --------------- FUNCTIONS -------------------------
+
+function services(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        midContainer.innerHTML += `<button class='services'>${arr[i]}</button>`
+    }
+    midContainer.innerHTML += `<p><span>TASK</span> <span>TOTAL</span></p>`
+}
+
+services(servicesArr)
+
+//-----------------------------------------------------------
+const serviceBtns = document.querySelectorAll('.services')
+
+for (let i = 0; i < serviceBtns.length; i++) {
+    serviceBtns[i].addEventListener('click', function() {
+        console.log(serviceBtns[i].textContent);
+
+    })
+}
