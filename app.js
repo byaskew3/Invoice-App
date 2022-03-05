5552d5c18fbfc95262c5171cf3cbeccea5b4a402
byaskew3 <byaskew3@gmail.com>
2022-03-05 15:01:57 -0700

    JS

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const midContainer = document.querySelector('.mid-container');
 
 // --------------- GLOBAL VARIABLES --------------
 
-const servicesArr = ['Wash Car: $10', 'Mow Lawn: $20', 'Pull Weeds: $30']
+const servicesArr = [{
+    service: 'Wash Car: $',
+    cost: 10
+}, {
+    service: 'Mow Lawn: $',
+    cost: 20
+}, {
+    service: 'Pull Weeds: $',
+    cost: 30
+}
+]
+
 let serviceItems = [];
 
 // ---------------- EVENT LISTENERS ---------------------
@@ -16,7 +27,7 @@ let serviceItems = [];
 
 function services(arr) {
     for (let i = 0; i < arr.length; i++) {
-        midContainer.innerHTML += `<button class='services'>${arr[i]}</button>`
+        midContainer.innerHTML += `<button class='services'>${arr[i].service}${arr[i].cost}</button>`
     }
     midContainer.innerHTML += `<p><span>TASK</span> <span>TOTAL</span></p>`
 }
@@ -29,8 +40,13 @@ const serviceBtns = document.querySelectorAll('.services')
 for (let i = 0; i < serviceBtns.length; i++) {
     serviceBtns[i].addEventListener('click', function() {
         const node = document.createElement('p');
+        node.classList.add('added-service');
         const textNode = document.createTextNode(`${serviceBtns[i].textContent}`)
-        node.append(textNode);
+        const spanNode = document.createElement('span')
+        spanNode.classList.add('remove')
+        const spanText = document.createTextNode('Remove')
+        spanNode.append(spanText)
+        node.append(textNode, spanNode);
         midContainer.appendChild(node)  
     })
 }
