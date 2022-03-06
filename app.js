df7b778cb1b729b5997141b49f840447dc93868e
byaskew3 <byaskew3@gmail.com>
2022-03-05 20:47:33 -0700

    JS

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // --------- CACHING THE DOM ----------------
-
 const midContainer = document.querySelector('.mid-container');
 const innerContainer = document.querySelector('.inner-container');
 
 
 // --------------- GLOBAL VARIABLES --------------
-
 const servicesArr = [{
     service: 'Wash Car',
     cost: 10
@@ -19,16 +17,11 @@ const servicesArr = [{
 ]
 
 let servicesAdded = false;
+let duplicateItems = false;
 let serviceItems = [];
 let totalCost = 0;
 
-
-// ---------------- EVENT LISTENERS ---------------------
-
-
-
 // --------------- FUNCTIONS -------------------------
-
 function services(arr) {
     for (let i = 0; i < arr.length; i++) {
         midContainer.innerHTML += `<button class='services'>${arr[i].service}: $${arr[i].cost}</button>`
@@ -48,36 +41,53 @@ function updateTotalCost(event) {
     }
 }
 
+function checkDuplicate(event) {
+    if (serviceItems.includes(`${event}`)) {
+        alert('You have already added this item!')
+        duplicateItems = true;
+    }
+}
+
 //-----------------------------------------------------------
 const serviceBtns = document.querySelectorAll('.services')
 
-for (let i = 0; i < serviceBtns.length; i++) {
-    serviceBtns[i].addEventListener('click', function() {
-        serviceItems.push(serviceBtns[i])
+function runApp() {
+    for (let i = 0; i < serviceBtns.length; i++) {
+        serviceBtns[i].addEventListener('click', function() {
+            checkDuplicate(serviceBtns[i].textContent)
+            if (duplicateItems) {
+                return false;
+            }
+            serviceItems.push(serviceBtns[i].textContent)
+            console.log(serviceItems)
+            
+            updateTotalCost(serviceBtns[i])
+    
+            const node = document.createElement('p');
+            node.classList.add('added-service');
+            const textNode = document.createTextNode(`${serviceBtns[i].textContent}`)
+            
+            const spanNode = document.createElement('span')
+            spanNode.classList.add('remove')
+            const spanText = document.createTextNode('Remove')
+            
+            
+            spanNode.append(spanText)
+            
+            node.append(textNode, spanNode);
+            
+            midContainer.appendChild(node)
+            
+            if (serviceItems.length === 1) {
+                servicesAdded = true;
+                innerContainer.innerHTML += `<p class="total"><span class="payment-method">We accept cash, credit card, or Paypal</span><span class='total-cost'></span></p>`
+            }
 
-        updateTotalCost(serviceBtns[i])
-
-        const node = document.createElement('p');
-        node.classList.add('added-service');
-        const textNode = document.createTextNode(`${serviceBtns[i].textContent}`)
-        
-        const spanNode = document.createElement('span')
-        spanNode.classList.add('remove')
-        const spanText = document.createTextNode('Remove')
-        
-        
-        spanNode.append(spanText)
-        
-        node.append(textNode, spanNode);
-        
-        midContainer.appendChild(node)
-        
-        if (serviceItems.length === 1) {
-            servicesAdded = true;
-            innerContainer.innerHTML += `<p class="total"><span class="payment-method">We accept cash, credit card, or Paypal</span><span class='total-cost'></span></p>`
-        }
-        let totalEl = document.querySelector('.total-cost')
-        totalEl.textContent = ` $${totalCost}`;
-    })
+            let totalEl = document.querySelector('.total-cost')
+            totalEl.textContent = ` $${totalCost}`;
+        })
+    }
 }
+
+runApp();
 
