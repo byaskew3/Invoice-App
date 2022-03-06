aafed7d46594c6997d80a064858ee5144c0b2f3b
byaskew3 <byaskew3@gmail.com>
2022-03-05 20:09:52 -0700

    JS

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const servicesArr = [{
 
 let servicesAdded = false;
 let serviceItems = [];
+let totalCost = 0;
 
 
 // ---------------- EVENT LISTENERS ---------------------
@@ -37,13 +38,25 @@ function services(arr) {
 
 services(servicesArr)
 
+function updateTotalCost(event) {
+    if (event.textContent === 'Wash Car: $10') {
+        totalCost += servicesArr[0].cost;
+    } else if (event.textContent === 'Mow Lawn: $20') {
+        totalCost += servicesArr[1].cost;
+    } else {
+        totalCost += 30;
+    }
+}
+
 //-----------------------------------------------------------
 const serviceBtns = document.querySelectorAll('.services')
 
 for (let i = 0; i < serviceBtns.length; i++) {
     serviceBtns[i].addEventListener('click', function() {
         serviceItems.push(serviceBtns[i])
-        console.log(serviceItems)
+
+        updateTotalCost(serviceBtns[i])
+
         const node = document.createElement('p');
         node.classList.add('added-service');
         const textNode = document.createTextNode(`${serviceBtns[i].textContent}`)
@@ -61,7 +74,10 @@ for (let i = 0; i < serviceBtns.length; i++) {
         
         if (serviceItems.length === 1) {
             servicesAdded = true;
-            innerContainer.innerHTML += `<p class="total"><span class="payment-method">We accept cash, credit card, or Paypal</span>$0</p>`
+            innerContainer.innerHTML += `<p class="total"><span class="payment-method">We accept cash, credit card, or Paypal</span><span class='total-cost'></span></p>`
         }
+        let totalEl = document.querySelector('.total-cost')
+        totalEl.textContent = ` $${totalCost}`;
     })
 }
+
